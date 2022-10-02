4438b40f0e681aaf66e6c3bffa19a8744a2a713d
Serhii Kushneruk <45001549+svkushneruk@users.noreply.github.com>
2022-10-02 20:45:01 +0300

    make step-2

---
+++ contacts.js
@@ -0,0 +1,51 @@
+const fs = require('fs/promises');
+const path = require("path");
+const {nanoid} = require('nanoid');
+
+const contactsPath = path.join(__dirname, "./db/contacts.json");
+
+const writeContacts = async (contacts) => await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+async function  listContacts() {
+    const contacts = await fs.readFile(contactsPath);
+    return JSON.parse(contacts);
+  }
+  
+  async function getContactById(contactId) {
+    const contacts = await listContacts();
+    const contact = contacts.find( ({id}) => id === contactId);
+    return contact || null;
+  }
+  
+  async function removeContact(contactId) {
+    const contacts = await listContacts();
+    const index = contacts.findIndex(contact => contact.id ===contactId );
+    if(index === -1) {
+      return null;
+    }
+    const [result] = contacts.splice(index,1)
+    console.log(result);
+    await writeContacts(contacts);
+    return result;
+  }
+  
+  async function addContact(name, email, phone) {
+    const newContact = {
+      id: nanoid(),
+      name,
+      email,
+      phone
+    }
+    const contacts = await listContacts();
+    contacts.push(newContact);
+    await writeContacts(contacts);
+    console.log(newContact);
+    return newContact;
+  }
+
+module.exports ={
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact
+}
